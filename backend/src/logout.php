dad4b4fed52bde5faae050ee16f91f0d67e2d214
Ingo Schwarzhaupt <ingo.schwarzhaupt@gmail.com>
2025-06-08 19:42:42 +0200

    Git logout

--- backend/src/logout.php
+++ backend/src/logout.php
@@ -1,5 +1,12 @@
 <?php
-
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 session_start();
 session_destroy();
 header('Content-Type: application/json');
